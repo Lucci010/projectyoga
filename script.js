86061dddf19fa965f06cfc3f4609219fc55acd73
Lucci010 <128175939+Lucci010@users.noreply.github.com>
2023-03-17 14:23:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector("nav svg")
+
+burger.addEventListener("click", () => {
+   if(burger.classList.contains('active')){
+    gsap.to(".links", {x: "100%"});
+    gsap.to(".line", {stroke: "white" });
+   }
+   else {
+    gsap.to('.links', {x: "0%"});
+    gsap.to(".line", {stroke: "black"});
+   }
+    burger.classList.toggle("active");
+})
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, {opacity: 0 });
+
+videos.forEach((video) => {
+    ScrollTrigger.create({
+        trigger: video,
+        start: "top center",
+        end: "bottom center",
+  markers: true,   
+  onEnter: () => {
+    gsap.to(video, {opacity: 1})
+    video.play();
+  },
+
+  onEnterBack: () => video.play(),
+  onLeave: () => video.pause(),
+onLeaveBack: () => video.pause(),
+    });
+});
